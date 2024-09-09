a443b9a792a95193ed58a96cf8f5c5af3f9058ec
Casmir Onyeka <casmir293@gmail.com>
2024-09-09 10:32:42 +0100

    feat: installed tailwind, and vuetify.

--- app.vue
+++ app.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <button
+    class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3"
+  >
+    ...testing
+  </button>
+  <v-btn> Button </v-btn>
 </template>
+
+<script setup></script>
---
+++ plugins/vuetify.ts
@@ -0,0 +1,15 @@
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import { aliases, mdi } from "vuetify/iconsets/mdi";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const vuetify = createVuetify({
+    icons: {
+      defaultSet: "mdi",
+      aliases,
+      sets: { mdi },
+    },
+  });
+  nuxtApp.vueApp.use(vuetify);
+});
