30061f4e905003965c991a5a57f0887b3c1ee6d4
Casmir Onyeka <casmir293@gmail.com>
2024-09-09 12:48:17 +0100

    feat: installed pinia, firebase, and toast.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,31 +3,52 @@ import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
+
   devtools: { enabled: true },
+
   css: ["~/assets/css/main.css"],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
   build: {
     transpile: ["vuetify"],
   },
+
   modules: [
+    "@pinia/nuxt",
+    "@pinia-plugin-persistedstate/nuxt",
     (_options, nuxt) => {
       nuxt.hooks.hook("vite:extendConfig", (config) => {
         // @ts-expect-error
         config.plugins.push(vuetify({ autoImport: true }));
       });
     },
-    //...
   ],
+
+  plugins: [{ src: "~/plugins/toast.ts", mode: "client" }],
+
   vite: {
     vue: {
       template: {
         transformAssetUrls,
       },
+    },
+  },
+
+  runtimeConfig: {
+    public: {
+      VITE_FIREBASE_API_KEY: process.env.VITE_FIREBASE_API_KEY,
+      VITE_FIREBASE_AUTH_DOMAIN: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+      VITE_FIREBASE_PROJECT_ID: process.env.VITE_FIREBASE_PROJECT_ID,
+      VITE_FIREBASE_STORAGE_BUCKET: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+      VITE_FIREBASE_MESSAGING_SENDER_ID:
+        process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+      VITE_FIREBASE_APP_ID: process.env.VITE_FIREBASE_APP_ID,
     },
   },
 });
