be67de90d93270ab687556f4d75a42f992f66aba
Casmir Onyeka <casmir293@gmail.com>
2024-09-19 15:28:19 +0100

    feat: removed skipping of middleware on initial client load only.

--- middleware/me.global.ts
+++ middleware/me.global.ts
@@ -1,11 +1,11 @@
 export default defineNuxtRouteMiddleware(async (to) => {
   const nuxtApp = useNuxtApp();
-  if (
-    import.meta.client &&
-    nuxtApp.isHydrating &&
-    nuxtApp.payload.serverRendered
-  )
-    return;
+  // if (
+  //   import.meta.client &&
+  //   nuxtApp.isHydrating &&
+  //   nuxtApp.payload.serverRendered
+  // )
+  //   return;
 
   const excludedRoutes = ["/auth/logout"];
   const { USER, userId, whoAmI } = useAuth();
