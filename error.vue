d1db65b77cff57b819552d29556db4ffc6840dff
Casmir Onyeka <casmir293@gmail.com>
2024-09-19 12:57:24 +0100

    final commit.

--- error.vue
+++ error.vue
@@ -8,7 +8,7 @@ defineProps({
 
 <template>
   <section class="flex h-dvh flex-col items-center justify-center">
-    <h1 class="font-sans text-3xl font-bold">⌛ Plan & Do ⌛</h1>
+    <h1 class="font-sans text-3xl font-bold">⌛ Task Manager ⌛</h1>
     <p class="mb-10 border-b-2 font-thin">The ultimate schedule manager</p>
     <main class="w-[90%]">
       <section class="flex flex-col items-center justify-center">
